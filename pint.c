e00d34178d613d670e090952fa543fb70d1f4a92
natashafeyh4 <natashefeyh44@gmail.com>
2023-10-19 18:37:13 +0000

    pint.c

--- pint.c
+++ pint.c
@@ -1,11 +1,19 @@
 #include "monty.h"
 
 /**
- * pint - Prints the value at the top of the stack, followed by a new line
+ * pint - Prints the value at the top of the stack, followed by a new line.
  * @stack: A pointer to the top of the stack
  * @line_number: The current line number in the Monty bytecode file
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    /* Your implementation for pint opcode here */
+	stack_t *top = *stack;
+
+	if (!top)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", top->n);
 }
