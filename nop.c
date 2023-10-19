104ccdf862d0fce5be160aee7342c14eb57d18b1
natashafeyh4 <natashefeyh44@gmail.com>
2023-10-19 19:24:08 +0000

    nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing
+ * @stack: Double pointer to the beginning of the stack (unused)
+ * @line_number: Current line number in the Monty bytecode file
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
